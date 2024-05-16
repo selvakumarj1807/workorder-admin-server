10f3efb7c5d2f18d58db4a071029687ad7700712
selva <selvakumarj1807@gmail.com>
2024-05-16 18:42:22 +0530

    3

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const app = express();
+const errorMiddleware = require('./middlewares/error');
+const cookieParser = require('cookie-parser')
 
+app.use(express.json());
+
+app.use(cookieParser());
+
+const acknowledgement = require('./routes/vendor-management/acknowledgement')
+const status = require('./routes/vendor-management/status')
+const history = require('./routes/vendor-management/history')
+
+app.use('/api/v1/vendor', acknowledgement);
+app.use('/api/v1/vendor', status);
+app.use('/api/v1/vendor', history);
+
+app.use(errorMiddleware);
 
 module.exports = app; 
