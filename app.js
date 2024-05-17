5a5a58ca5ad872a6d3ade8509de8744c8ebe68c6
selva <selvakumarj1807@gmail.com>
2024-05-17 11:21:40 +0530

    4

--- app.js
+++ app.js
@@ -10,10 +10,16 @@ app.use(cookieParser());
 const acknowledgement = require('./routes/vendor-management/acknowledgement')
 const status = require('./routes/vendor-management/status')
 const history = require('./routes/vendor-management/history')
+const invoice = require('./routes/vendor-management/invoice')
+const payment = require('./routes/vendor-management/payment')
+
 
 app.use('/api/v1/vendor', acknowledgement);
 app.use('/api/v1/vendor', status);
 app.use('/api/v1/vendor', history);
+app.use('/api/v1/vendor', invoice);
+app.use('/api/v1/vendor', payment);
+
 
 app.use(errorMiddleware);
 
