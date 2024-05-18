5d1d231ece18d8ea88642e970e7c853b1fbafd8b
selva <selvakumarj1807@gmail.com>
2024-05-18 17:04:28 +0530

    7

--- app.js
+++ app.js
@@ -28,19 +28,19 @@ app.use('/api/v1/vendor', productDeliveryHistory);
 const acknowledgementUser = require('./routes/user-management/acknowledgement')
 const statusUser = require('./routes/user-management/status')
 const historyUser = require('./routes/user-management/history')
-//const invoice = require('./routes/vendor-management/invoice')
-//const payment = require('./routes/vendor-management/payment')
-//const paymentHistory = require('./routes/vendor-management/paymentHistory')
-//const productDeliveryHistory = require('./routes/vendor-management/productDeliveryHistory')
+const invoiceUser = require('./routes/user-management/invoice')
+const paymentUser = require('./routes/user-management/payment')
+const paymentHistoryUser = require('./routes/user-management/paymentHistory')
+const productDeliveryHistoryUser = require('./routes/user-management/productDeliveryHistory')
 
 
 app.use('/api/v1/user', acknowledgementUser);
 app.use('/api/v1/user', statusUser);
 app.use('/api/v1/user', historyUser);
-//app.use('/api/v1/vendor', invoice);
-//app.use('/api/v1/vendor', payment);
-//app.use('/api/v1/vendor', paymentHistory);
-//app.use('/api/v1/vendor', productDeliveryHistory);
+app.use('/api/v1/user', invoiceUser);
+app.use('/api/v1/user', paymentUser);
+app.use('/api/v1/user', paymentHistoryUser);
+app.use('/api/v1/user', productDeliveryHistoryUser);
 
 
 app.use(errorMiddleware);
